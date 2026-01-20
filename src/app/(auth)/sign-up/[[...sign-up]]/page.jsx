5488d1873b1ba2e3c8ab32caa1a039781604ff96
Md. Zihad <md.zihad0246@gmail.com>
2026-01-21 03:30:09 +0600

    style: Wrap SignUp component in a styled div for improved layout

--- src/app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,5 +1,7 @@
 import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignUp />
+  return <div className = "bg-amber-300 flex justify-center p-5">
+    <SignUp />
+  </div>
 }
